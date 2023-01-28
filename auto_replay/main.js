a7044a01bbebbbd133a9cdc9bf2f9d6dd84b25e1
Tanvir Ahmed <tanvirpoly@gmail.com>
2023-01-28 22:54:00 +0600

    project

---
+++ auto_replay/main.js
@@ -0,0 +1,63 @@
+const texts = document.querySelector('.texts');
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+
+recognition.addEventListener('result', (e)=>{
+  texts.appendChild(p);
+  const text = Array.from(e.results)
+  .map(result => result[0])
+  .map(result => result.transcript)
+  .join('');
+
+  p.innerText = text;
+  if(e.results[0].isFinal){
+    if (text.includes('how are you')) {
+      p = document.createElement('p');
+      p.classList.add('replay');
+      p.innerText = 'I am fine';
+      texts.appendChild(p)
+    }
+
+
+
+      if (text.includes("Hello") || text.includes('hello')) {
+      p = document.createElement('p');
+      p.classList.add('replay');
+      p.innerText = 'Hi, How are You?';
+      texts.appendChild(p)
+    }
+
+
+
+
+
+    if (text.includes("what's your name") || text.includes('what is your name')) {
+      p = document.createElement('p');
+      p.classList.add('replay');
+      p.innerText = 'My Name is Tanvir';
+      texts.appendChild(p)
+    }
+
+    if (text.includes('open my YouTube')) {
+      p = document.createElement('p');
+      p.classList.add('replay');
+      p.innerText = 'open youtube';
+      texts.appendChild(p)
+      console.log('opening YouTube')
+      window.open('https://www.youtube.com/')
+    }
+    p = document.createElement('p');
+  }
+});
+
+
+recognition.addEventListener('end', ()=>{
+  recognition.start();
+})
+
+recognition.start();
